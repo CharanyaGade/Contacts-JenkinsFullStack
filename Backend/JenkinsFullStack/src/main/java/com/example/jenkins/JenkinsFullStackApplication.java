1c1a54ff0d1f42ae092a9a4eaf7c97be700cb253
Pujitha <pujithashaik93@gmail.com>
2025-09-13 22:03:53 +0530

    modified javafullstack

--- Backend/JenkinsFullStack/src/main/java/com/example/jenkins/JenkinsFullStackApplication.java
+++ Backend/JenkinsFullStack/src/main/java/com/example/jenkins/JenkinsFullStackApplication.java
@@ -2,9 +2,16 @@ package com.example.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class JenkinsFullStackApplication {
+public class JenkinsFullStackApplication extends SpringBootServletInitializer  {
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinsFullStackApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsFullStackApplication.class, args);
